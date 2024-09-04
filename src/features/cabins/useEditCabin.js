9cd9e785400c308033596967106ad5fabd841b53
Asante.dev <davidameyaw607@gmail.com>
2024-09-03 22:15:21 -0800

    hook

---
+++ src/features/cabins/useEditCabin.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createEditCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+
+export function useEditCabin() {
+  const queryClient = useQueryClient();
+
+  const { mutate: editCabin, isLoading: isEditing } = useMutation({
+    mutationFn: ({ newCabin, id }) => createEditCabin(newCabin, id),
+    onSuccess: () => {
+      toast.success("New cabin successfully created");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { editCabin, isEditing };
+}
