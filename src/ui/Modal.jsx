f37d02821cffbd6c80e45886a6418f271df26ad4
Asante.dev <davidameyaw607@gmail.com>
2024-09-27 21:13:27 -0800

    Modal

---
+++ src/ui/Modal.jsx
@@ -0,0 +1,122 @@
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { createPortal } from "react-dom";
+import { HiXMark } from "react-icons/hi2";
+import styled from "styled-components";
+
+const StyledModal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-lg);
+  box-shadow: var(--shadow-lg);
+  padding: 3.2rem 4rem;
+  transition: all 0.5s;
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: var(--backdrop-color);
+  backdrop-filter: blur(4px);
+  z-index: 1000;
+  transition: all 0.5s;
+`;
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  padding: 0.4rem;
+  border-radius: var(--border-radius-sm);
+  transform: translateX(0.8rem);
+  transition: all 0.2s;
+  position: absolute;
+  top: 1.2rem;
+  right: 1.9rem;
+
+  &:hover {
+    background-color: var(--color-grey-100);
+  }
+
+  & svg {
+    width: 2.4rem;
+    height: 2.4rem;
+    /* Sometimes we need both */
+    /* fill: var(--color-grey-500);
+    stroke: var(--color-grey-500); */
+    color: var(--color-grey-500);
+  }
+`;
+
+const modalContext = createContext();
+//eslint-disable-next-line
+function Modal({ children }) {
+  //1.Create a context
+  //eslint-disable-next-line
+  const [openName, setOpenName] = useState("");
+
+  const close = () => setOpenName("");
+  const open = setOpenName;
+
+  return (
+    <modalContext.Provider value={{ open, close, openName }}>
+      {children}
+    </modalContext.Provider>
+  );
+  //Updating logic and state
+}
+
+//children components
+function Open({ children, opens }) {
+  const { open } = useContext(modalContext);
+
+  return <div>{cloneElement(children, { onClick: () => open(opens) })}</div>;
+}
+function Window({ children, name }) {
+  const { openName, close } = useContext(modalContext);
+  const ref = useRef();
+
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) {
+          console.log(e), close();
+        }
+      }
+
+      document.addEventListener("click", handleClick, true);
+
+      return document.removeEventListener("click", handleClick);
+    },
+    [close]
+  );
+  if (name !== openName) return null;
+
+  return createPortal(
+    <Overlay>
+      <StyledModal ref={ref}>
+        <Button onClick={close}>
+          <HiXMark />
+        </Button>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
+      </StyledModal>
+    </Overlay>,
+    document.body
+  );
+}
+//add the children as properties
+Modal.Open = Open;
+Modal.Window = Window;
+
+export default Modal;
